b11f5b4e8faa74e4eebc54d444c6599fb2c72382
Ericadp.developer <ericadp.developer@gmail.com>
2020-08-09 21:38:27 -0400

    Create header.js

---
+++ header.js
@@ -0,0 +1,18 @@
+/*
+============================================
+; Title:  header.js
+; Author: Eria Perry
+; Date: 4 June 2020
+; Description: Displays a formatted header
+;===========================================
+*/
+
+/*
+* Params: firstName, lastName, assignment
+* Response: output 
+* Description: Returns a well-formatted string header
+*/
+
+const header = required('./perry-header.js');
+
+console.log(header.display("Erica","Perry","week 2"));
